bb1515c2d40495277e7aedb8e7362f60b444ade8
Mónica Espiñeira <monica.espineira1988@gmail.com>
2025-12-09 13:56:25 +0100

    actualizacion

--- 3.Modulo Profesional Optativo/Tema 5/Agenda/src/Main.java
+++ 3.Modulo Profesional Optativo/Tema 5/Agenda/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    // ejercicio para acabar nosotros. Agenda de contactos-> Estrucctura de datos en ArrayList<> 
+    // ejercicio para acabar nosotros. Agenda de contactos-> Estrucctura de datos en ArrayList<>
+    
     // necesito poder guardar tantos contactos como necesite
     // cada contacto tiene unos datos fijos -> nombre , apellido , dni y teléfono -> Object[]
     // Object[]{"Borja","Martin","12324a",622016513}
